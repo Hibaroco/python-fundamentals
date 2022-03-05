b5969ff570c5f69547c5dceaf498dce59e2624b7
Jesse Hibarger <jhibarger@wsutech.edu>
2022-03-05 17:30:31 -0600

    CAC-103-3

---
+++ exercises/exercise2.py
@@ -0,0 +1,53 @@
+def simple():
+    print("Hut!")
+
+
+value1 = "Red"
+value2 = 43
+value3 = "Green"
+value4 = 75
+value5 = "Set"
+print(value1)
+print(value2)
+print(value3)
+print(value4)
+print(value5)
+
+
+simple()
+
+
+def simple2():
+
+
+    print("Here we go!")
+
+
+value6 = "The Quarterback drops back"
+print(value6)
+value6 = "Rodgers is going deep!"
+print(value6)
+value6 = "He sets up for the Hail Mary"
+print(value6)
+value6 = "Rodgers jumps for the catch and Touchdown!"
+print(value6)
+
+
+simple2()
+
+
+def message(name):
+    print("Man, I cannot wait for " + name + " to come out!")
+
+
+
+message("Moon Knight")
+
+
+
+
+
+
+
+
+
