084fa8ca53db72a9d180554a0a9819e9393a2ceb
Jesse Hibarger <jhibarger@wsutech.edu>
2022-03-08 12:47:56 -0600

    Exercise3.py

---
+++ exercises/exercise3.py
@@ -0,0 +1,26 @@
+first_name = 'Bruce'
+message_1 = f"Hey, {first_name} would you like to be in a message?"
+print(message_1)
+
+name = "bruce banner"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = "Michael Scott once said, """'"You miss 100% of the shots you do not take."'""" -Wayne Gretsky"""
+print(quote)
+
+
+famous_person = "Michael Scott"
+message = f"{famous_person} once said, """'"You miss 100% of the shots you do not take."'""" -Wayne Gretsky"""
+print(message)
+
+
+spiderman = " Peter Parker "
+print(spiderman)
+print(spiderman.lstrip())
+print(spiderman.rstrip())
+print(spiderman.strip())
+print("\n"'spiderman')
+print("\t"'spiderman')
+
