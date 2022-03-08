288ea7d14a0f20e184fea78a07426f40bfb66037
Jesse Hibarger <jhibarger@wsutech.edu>
2022-03-08 15:39:39 -0600

    Exercise4 - Numbers

---
+++ exercises/exercise4.py
@@ -0,0 +1,29 @@
+print(2*4)
+print(10-2)
+print(4+4)
+print(16/2)
+
+fav_num = 12
+message = 'My favorite number is:'
+print(message, (fav_num))
+
+value_1 = 456234
+value_2 = 68423791
+value_3 = 13794628
+value_4 = 96374
+
+number_sets = value_1, value_2, value_3, value_4
+print(number_sets)
+
+
+value_6 = float(65)
+value_7 = int(83)
+func_1 = (value_6),(value_7)
+
+print(func_1)
+
+movie = input('What is your favorite movie?')
+seen = input('How many times have you seen it?')
+message_1 = 'I have seen {0} {1} times'
+print(message_1.format(movie, seen))
+
