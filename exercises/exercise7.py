82fffe9b1d67c6d444b82fd8a5aec03938861c49
Jesse Hibarger <jhibarger@wsutech.edu>
2022-03-13 13:16:10 -0500

    Exercise 7 - Loops

---
+++ exercises/exercise7.py
@@ -0,0 +1,35 @@
+# 4-3 Counting to 20
+def count():
+    for number in range(1, 21):
+        print(number)
+
+
+count()
+# 4-6 Odd Numbers
+
+
+def count_2():
+    for number in range(1, 21, 2):
+        print(number)
+
+
+count_2()
+
+# 4-7 Threes
+def threes():
+    for number in range(3, 31, 3):
+        print(number)
+
+
+threes()
+# 4-8 Cubes
+
+
+def cubes():
+    for number in range(1, 11):
+        print(number**3)
+
+
+cubes()
+
+
